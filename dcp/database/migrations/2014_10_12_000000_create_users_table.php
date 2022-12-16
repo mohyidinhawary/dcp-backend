3ea492cc560f15d78a902827d6e363f50a4e6850
RamiH20 <120262594+RamiH20@users.noreply.github.com>
2022-12-16 18:20:57 +0300

    Update 2014_10_12_000000_create_users_table.php

--- dcp/database/migrations/2014_10_12_000000_create_users_table.php
+++ dcp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->Biginteger('wallet')->default(100);
+            $table->time('shiftStart');
+            $table->time('shiftEnd');
             $table->rememberToken();
-            $table->timestamps();
+             //$table->timestamp('email_verified_at')->nullable();
         });
     }
 
