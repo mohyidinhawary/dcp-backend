5bd8e15592a382a9bb034cadf13f393e19d25988
RamiH20 <120262594+RamiH20@users.noreply.github.com>
2022-12-16 18:19:33 +0300

    Update ExpertController.php

--- dcp/app/Http/Controllers/api/ExpertController.php
+++ dcp/app/Http/Controllers/api/ExpertController.php
@@ -5,55 +5,68 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Expert;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ExpertController extends Controller
 {
    // this is an api for expert register and the http request method will be (post)
-   public function Register(Request $request){
-    //validation
-    $request->validate([
-        "name"=>"required",
-        "email"=>"required|email|unique:experts",
-        "password"=>"required|confirmed"
-    ]);
-    // create expert+save
-    $expert=new Expert();
-    $expert->name=$request->name;
-    $expert->email=$request->email;
-    $expert->password=bcrypt($request->password);
-    $expert->save();
-    // send response
-    return response()->json([
-    'status'=>1,
-    'massege'=>'expert registered succsessfully'
-    
-    ],200);
-        }
+    public function Register(Request $request){
+        //validation
+        $request->validate([
+            "name"=>"required",
+            "email"=>"required|email|unique:experts",
+            "password"=>"required|confirmed"
+        ]);
+        // create expert+save
+        $expert=new Expert();
+        $expert->name=$request->name;
+        $expert->email=$request->email;
+        $expert->password=bcrypt($request->password);
+        $expert->phone_number=$request->phone_number;
+        $expert->address=$request->address;
+        $expert->role=$request->role;
+        $expert->experiences=$request->experiences;
+        $expert->save();
+        // send response
+        return response()->json([
+        'status'=>1,
+        'massege'=>'expert registered succsessfully'
+        
+        ],200);
+    }
 
-        //this an api for expert login (Post)
-        public function Login(Request $request){
-            //validation
-            $request->validate([
-                "email"=>"required|email",
-                "password"=>"required",
-                //verify expert + token
-                
-            ]);
-             //verify  + token
-            if(!$token=auth()->attempt(["email"=>$request->email,"password"=>$request->password])){
-                return response()->json([
-                    'status'=>0,
-                    'message'=>'invaild'
+    //this an api for expert login (Post)
+    public function Login(Request $request){
+        $request -> validate([
+            "email" => "required|email",
+            "password" =>"required"
+        ]);
+        $expert = Expert::where("email" , "=" , $request -> email)-> first();
+
+        if(isset($expert->id) ){
+            if(Hash::check($request -> password , $expert ->password)){
+               $token1 = $expert -> createToken("API TOKEN")->plainTextToken;
+                return response() ->json([
+                    "status" =>1,
+                    'massege'=>"Expert logged in",
+                    "token"=>$token1
                 ]);
             }
-                // response
-                return response()->json([
-                    'status'=>1,
-                    'message'=>'logged in successfully',
-                    'token'=>$token
+            else
+            {
+                return response() ->json([
+                    "status"=>0,
+                    'massege'=>"wrong password"
                 ]);
             }
-        //this is an api for expert logout (get)
+        }
+        else{
+        return response() -> json([
+            "status" => 0,
+            'massege'=>"Expert not found"
+        ],404);
+    }
+    } //this is an api for expert logout (get)
     public function Logout(){
         auth()->Logout();
         return response()->json([
@@ -61,5 +74,13 @@ class ExpertController extends Controller
             'message'=>'user logged out '
         ]);
         
+    }
+    public function Profile(){
+        $expert_data=auth()->user();
+        return response()->json([
+            'status'=>1,
+            'message'=>'expert profile data',
+            'use data'=>$expert_data
+        ]);
     }
 }
