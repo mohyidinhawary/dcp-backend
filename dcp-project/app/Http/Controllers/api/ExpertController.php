a45f9069393bbb1a0b84b540d5b449db88341d80
RamiH20 <120262594+RamiH20@users.noreply.github.com>
2022-12-22 00:50:57 +0300

    Update ExpertController.php

--- dcp-project/app/Http/Controllers/api/ExpertController.php
+++ dcp-project/app/Http/Controllers/api/ExpertController.php
@@ -19,12 +19,12 @@ class ExpertController extends Controller
         "address"=>"required",
         "experiences"=>"required",
         "experience_years"=>"required",
+        "session_price"=>"required",
         "role1"=>"",
         "role2"=>"",
         "role3"=>"",
         "role4"=>"",
         "role5"=>"",
-
     ]);
     // create expert+save
     $expert=new Expert();
@@ -35,6 +35,7 @@ class ExpertController extends Controller
     $expert->address=$request->address;
     $expert->experiences=$request->experiences;
     $expert->experience_years=$request->experience_years;
+    $expert->session_price=$request->session_price;
     $expert->role1=$request->role1;
     $expert->role2=$request->role2;
     $expert->role3=$request->role3;
@@ -47,31 +48,30 @@ class ExpertController extends Controller
     'massege'=>'expert registered succsessfully'
     
     ],200);
-}
+    }
 
         //this an api for expert login (Post)
-        public function Login(Request $request){
+    public function Login(Request $request){
             //validation
-            $request->validate([
-                "email"=>"required|email",
-                "password"=>"required",
-                //verify expert + token
-                
-            ]);
+        $request->validate([
+            "email"=>"required|email",
+            "password"=>"required",
+                //verify expert + token   
+        ]);
              //verify  + token
-            if(!$token=auth()->guard('expert-api')->attempt(["email"=>$request->email,"password"=>$request->password])){
-                return response()->json([
-                    'status'=>0,
-                    'message'=>'invaild'
-                ]);
-            }
+        if(!$token=auth()->guard('expert-api')->attempt(["email"=>$request->email,"password"=>$request->password])){
+            return response()->json([
+                'status'=>0,
+                'message'=>'invaild'
+            ]);
+        }
                 // response
-                return response()->json([
-                    'status'=>1,
-                    'message'=>'logged in successfully',
-                    'token'=>$token
-                ]);
-            }
+        return response()->json([
+            'status'=>1,
+            'message'=>'logged in successfully',
+            'token'=>$token
+        ]);
+    }
         //this is an api for expert logout (get)
     public function Logout(){
         auth()->guard('expert-api')->Logout();
