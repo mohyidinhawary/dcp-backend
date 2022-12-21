57c8ae1c5e33468ae0a2adea076ea3f7a961660d
RamiH20 <120262594+RamiH20@users.noreply.github.com>
2022-12-22 00:48:21 +0300

    Update UserController.php

--- dcp-project/app/Http/Controllers/api/UserController.php
+++ dcp-project/app/Http/Controllers/api/UserController.php
@@ -4,31 +4,34 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Expert;
+use App\Models\ExpertAvailability;
 use Illuminate\Http\Request;
-use App\Models\ExpertDetails;
 use Illuminate\Support\Facades\DB;
+use PDO;
+
 class UserController extends Controller
 {
     // this is an api for user register and the http request method will be (post)
     public function Register(Request $request){
-//validation
-$request->validate([
-    "name"=>"required",
-    "email"=>"required|email|unique:users",
-    "password"=>"required|confirmed"
-]);
-// create user+save
-$user=new User();
-$user->name=$request->name;
-$user->email=$request->email;
-$user->password=bcrypt($request->password);
-$user->save();
-// send response
-return response()->json([
-'status'=>1,
-'massege'=>'user registered succsessfully'
+        
+        $request->validate([
+            "name"=>"required",
+            "email"=>"required|email|unique:users",
+            "password"=>"required|confirmed",
+        ]);
+        // create user+save
+        $user=new User();
+        $user->name=$request->name;
+        $user->email=$request->email;
+        $user->password=bcrypt($request->password);
+        $user->save();
+        // send response
+        return response()->json([
+        'status'=>1,
+        'massege'=>'user registered succsessfully'
 
-],200);
+        ],200);
     }
     //  this is an api for user login (Post)
     public function Login(Request $request){
@@ -52,7 +55,7 @@ return response()->json([
                 'message'=>'logged in successfully',
                 'token'=>$token
             ]);
-        }
+    }
     
     //get
     public function Profile(){
@@ -94,23 +97,77 @@ return response()->json([
            
             'Consulting'=>[
                 "Medical_consultations"=>$experts1,
-                    "Professional_consulting"=>$experts2,
-                    "Psychological_counseling"=>$experts3,
-                    "Family_counseling"=>$experts4,
+                "Professional_consulting"=>$experts2,
+                "Psychological_counseling"=>$experts3,
+                "Family_counseling"=>$experts4,
                 "management_consulting"=>$experts5
             ]
         ]);
            
-            }
+    }
 
+    public function getExpertDetails($id){
+        $consultant = DB::table('experts')->where('experts.id',$id)->select('id','name','email','phone_number','address','role1','role2','role3','role4','role5','experiences','experience_years')
+            ->get();
+        return response()->json($consultant);
+    }
 
-
-
-            public function getExpertDetails($id){
- 
-                $consultant = DB::table('experts')->where('experts.id',$id)->select('id','name','email','phone_number','address','role','experiences','experience_years')
-                ->get();
-                 return response()->json($consultant);
-             }
-
+//payment methodes
+    public function addToWallet(Request $request){
+        $request->validate([
+            "email"=>"required|email",
+            "cash"=>"required"
+        ]);
+        $user=User::where('email',$request->email)->first();
+        $cash=$request->cash;
+        if(!$user){
+            return response()->json([
+                "message"=>"wronng email"
+            ]);
+        }
+        $user -> wallet+=$cash;
+        $user->save();
+        return response()->json([
+            "message"=>"cash has been added successfully"
+        ]);
+    } 
+    
+    
+    public function pay($id,Request $request){
+        $request->validate([
+            "email"=>"required|email"
+        ]);
+        $admin1=User::where('id',$id)->first();
+        $admin2=Expert::where('email',$request->email)->first();
+        $admin1->wallet-=($admin2->session_price);
+        $admin2->wallet+=($admin2->session_price);
+        $admin1->save();
+        $admin2->save();
+        return response()->json([
+            "message"=>"payment has been done successfully"
+        ]);
+    }
+//reservation methodes
+    public function AvailableTime($id){
+        $user=ExpertAvailability::where('expert_id',$id)->select('day','date','from','to')->get();
+        return response()->json([
+            "message"=>$user
+        ]);
+    }    
+    public function Reservation($id1,$id2){
+        $res1=ExpertAvailability::where('expert_id',$id2)->first();
+        if(!($res1->user_id)){
+            $res1->user_id=$id1;
+            $res1->save();
+            return response()->json([
+                "message"=>"done",
+                "message"=>$res1
+            ]);
+        }
+        else{
+            return response()->json([
+                "message"=>"already taken"
+            ]);
+        }
+    }                                                                  
 }
