7185cdb4a2d427e8c3ef220d721a8d2a9cd88ef8
RamiH20 <120262594+RamiH20@users.noreply.github.com>
2022-12-22 09:16:04 +0300

    Update ExpertController.php

--- dcp-project/app/Http/Controllers/api/ExpertController.php
+++ dcp-project/app/Http/Controllers/api/ExpertController.php
@@ -4,50 +4,54 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Expert;
+use App\Models\ExpertAvailability;
 use Illuminate\Http\Request;
+use Ramsey\Uuid\Type\Integer;
+use Illuminate\Support\Facades\DB;
+
 
 class ExpertController extends Controller
 {
    // this is an api for expert register and the http request method will be (post)
-   public function Register(Request $request){
-    //validation
-    $request->validate([
-        "name"=>"required",
-        "email"=>"required|email|unique:experts",
-        "password"=>"required|confirmed",
-        "phone_number"=>"required",
-        "address"=>"required",
-        "experiences"=>"required",
-        "experience_years"=>"required",
-        "session_price"=>"required",
-        "role1"=>"",
-        "role2"=>"",
-        "role3"=>"",
-        "role4"=>"",
-        "role5"=>"",
-    ]);
-    // create expert+save
-    $expert=new Expert();
-    $expert->name=$request->name;
-    $expert->email=$request->email;
-    $expert->password=bcrypt($request->password);
-    $expert->phone_number=$request->phone_number;
-    $expert->address=$request->address;
-    $expert->experiences=$request->experiences;
-    $expert->experience_years=$request->experience_years;
-    $expert->session_price=$request->session_price;
-    $expert->role1=$request->role1;
-    $expert->role2=$request->role2;
-    $expert->role3=$request->role3;
-    $expert->role4=$request->role4;
-    $expert->role5=$request->role5;
-    $expert->save();
-    
-    return response()->json([
-    'status'=>1,
-    'massege'=>'expert registered succsessfully'
-    
-    ],200);
+    public function Register(Request $request){
+        //validation
+        $request->validate([
+            "name"=>"required",
+            "email"=>"required|email|unique:experts",
+            "password"=>"required|confirmed",
+            "phone_number"=>"required",
+            "address"=>"required",
+            "experiences"=>"required",
+            "experience_years"=>"required",
+            "session_price"=>"required",
+            "role1"=>"",
+            "role2"=>"",
+            "role3"=>"",
+            "role4"=>"",
+            "role5"=>"",
+        ]);
+        // create expert+save
+        $expert=new Expert();
+        $expert->name=$request->name;
+        $expert->email=$request->email;
+        $expert->password=bcrypt($request->password);
+        $expert->phone_number=$request->phone_number;
+        $expert->address=$request->address;
+        $expert->experiences=$request->experiences;
+        $expert->experience_years=$request->experience_years;
+        $expert->session_price=$request->session_price;
+        $expert->role1=$request->role1;
+        $expert->role2=$request->role2;
+        $expert->role3=$request->role3;
+        $expert->role4=$request->role4;
+        $expert->role5=$request->role5;
+        $expert->save();
+        
+        return response()->json([
+        'status'=>1,
+        'massege'=>'expert registered succsessfully'
+        
+        ],200);
     }
 
         //this an api for expert login (Post)
@@ -81,4 +85,18 @@ class ExpertController extends Controller
         ]);
         
     }
+    public function ShowAvailableTimes($id){
+        $expert=ExpertAvailability::where('expert_id',$id)->get();
+        if(DB::table('experts-avilableity')->where('expert_id',$id)->select('user_id')->get()){
+            return response()->json([
+                "message"=>$expert
+            ]);
+        }
+        else{
+            return response()->json([
+                "message"=>"error"
+            ]);
+        }
+    }
+
 }
