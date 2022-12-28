2000072e40992a0f5a8f55b62886597fb1404cb8
RamiH20 <120262594+RamiH20@users.noreply.github.com>
2022-12-29 02:17:47 +0300

    Update UserController.php

--- dcp/app/Http/Controllers/api/UserController.php
+++ dcp/app/Http/Controllers/api/UserController.php
@@ -5,90 +5,260 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Expert;
+use App\Models\Rating;
+use App\Models\ExpertAvailability;
 use Illuminate\Http\Request;
-use App\Http\Controllers\api\ExpertController;
 use Illuminate\Support\Facades\DB;
+use PDO;
 
 class UserController extends Controller
 {
     // this is an api for user register and the http request method will be (post)
     public function Register(Request $request){
-        //validation
+        
         $request->validate([
             "name"=>"required",
             "email"=>"required|email|unique:users",
             "password"=>"required|confirmed",
-            "shiftStart"=>'required|date_format:H:i',
-            "shiftEnd"=>'required|date_format:H:i'
         ]);
         // create user+save
         $user=new User();
         $user->name=$request->name;
         $user->email=$request->email;
         $user->password=bcrypt($request->password);
-        $user->wallet=$request->wallet;
-        $user->shiftStart=$request->shiftStart;
-        $user->shiftEnd=$request->shiftEnd;
         $user->save();
         // send response
         return response()->json([
         'status'=>1,
         'massege'=>'user registered succsessfully'
-        ],200);
-        }
 
+        ],200);
+    }
     //  this is an api for user login (Post)
     public function Login(Request $request){
         //validation
         $request->validate([
             "email"=>"required|email",
-            "password"=>"required"
-        //verify user + token
-                
+            "password"=>"required",
+            //verify user + token
+            
         ]);
-        //verify user + token
-        if(!$token=auth()->attempt(["email"=>$request->email,"password"=>$request->password])){
+         //verify user + token
+        if(!$token=auth()->guard('user-api')->attempt(["email"=>$request->email,"password"=>$request->password])){
             return response()->json([
                 'status'=>0,
                 'message'=>'invaild'
             ]);
-            }
-        // response
+        }
+            // response
+            return response()->json([
+                'status'=>1,
+                'message'=>'logged in successfully',
+                'token'=>$token
+            ]);
+    }
+    //get
+    public function Profile(){
+        $user_data=auth()->user();
         return response()->json([
             'status'=>1,
-            'message'=>'logged in successfully',
-            'token'=>$token
-            ]);
-        }
+            'message'=>'user profile data',
+            'use data'=>$user_data
+        ]);
         
-        //get
-    
-    //  this is an api for user logout (get)
+    }
+//  this is an api for user logout (get)
     public function Logout(){
-        auth()->Logout();
+        auth()->guard('user-api')->Logout();
         return response()->json([
             'status'=>1,
             'message'=>'user logged out '
         ]);
         
     }
-    public function Profile(){
-        $user_data=auth()->user();
+    public function BrowseConsultingExperts(){
+        $experts1=DB::table('experts')->where('Medical_consulting','=',true)
+        ->select('name')
+        ->get();
+        $experts2=DB::table('experts')->where('Professional_consulting','=',true)
+        ->select('name')
+        ->get();
+        $experts3=DB::table('experts')->where('Psychological_consulting','=',true)
+        ->select('name')
+        ->get();
+        $experts4=DB::table('experts')->where('Family_consulting','=',true)
+        ->select('name')
+        ->get();
+        $experts5=DB::table('experts')->where('management_consulting','=',true)
+        ->select('name')
+        ->get();
         return response()->json([
-            'status'=>1,
-            'message'=>'user profile data',
-            'user data'=>$user_data
+           
+            'Consulting'=>[
+                "Medical_consultations"=>$experts1,
+                "Professional_consulting"=>$experts2,
+                "Psychological_counseling"=>$experts3,
+                "Family_counseling"=>$experts4,
+                "management_consulting"=>$experts5
+            ]
         ]);
+           
     }
+//get all experts
     public function getExpertsDetails(){
-       $consultant = Expert::query()->get();
+        $consultant = DB::table('experts')->select('id','name','email','phone_number','address','session_price','avg_rate','Medical_consulting','Professional_consulting','Psychological_consulting','Family_consulting','management_consulting','experiences','experience_years')
+            ->get();
         return response()->json($consultant);
     }
-    //getting details for an expert by id
+//get one expert
     public function getExpertDetails($id){
-        $consultant = Expert::where('experts.id',$id)->first();
-         return response()->json($consultant);
-     }
-    
-    
+        $consultant = DB::table('experts')->where('experts.id',$id)->select('id','name','email','phone_number','address','session_price','rate','Medical_consulting','Professional_consulting','Psychological_consulting','Family_consulting','management_consulting','experiences','experience_years')
+            ->get();
+        return response()->json($consultant);
+    }
+//look for an expert
+    public function search(Request $request){
+        $request->validate([
+           "name"=>"required"
+        ]);
+        $consultant= Expert::where('name', $request->name)->select('name')->first();
+        if(!($consultant)){
+            return response()->json([
+                'status'=>0,
+                'message'=>'no expert with the same name'
+            ]);
+        }
+        else
+        { 
+            $c=Expert::where('name', $request->name)->get();
+            return response()->json([
+                'status'=>1,
+                'Expert is'=> $c
+            ]);
+        }
+    }
+//payment methodes
+    public function addToWallet(Request $request){
+        $request->validate([
+            "email"=>"required|email",
+            "cash"=>"required"
+        ]);
+        $user=User::where('email',$request->email)->first();
+        $cash=$request->cash;
+        if(!$user){
+            return response()->json([
+                "message"=>"wronng email"
+            ]);
+        }
+        $user -> wallet+=$cash;
+        $user->save();
+        return response()->json([
+            "message"=>"cash has been added successfully"
+        ]);
+    } 
+    public function pay($id,Request $request){
+        $request->validate([
+            "email"=>"required|email"
+        ]);
+        // $admin1=User::where('id',$id)->first();
+        // $admin2=Expert::where('email',$request->email)->first();
+        // $admin1->wallet-=($admin2->session_price);
+        // $admin2->wallet+=($admin2->session_price);
+        // $admin1->save();
+        // $admin2->save();
+        if($admin1=User::where('wallet',0)->first()){
+            
+            return response()->json([
+                "message"=>"you cannot pay because your wallet is empty"
+            ]);
+        }
+        else{
+        $admin1=User::where('id',$id)->first();
+        $admin2=Expert::where('email',$request->email)->first();
+        $admin1->wallet-=($admin2->session_price);
+        $admin2->wallet+=($admin2->session_price);
+        $admin1->save();
+        $admin2->save();
+        
+        return response()->json([
+            "message"=>"payment has been done successfully"
+        ]);
+        }
+    }
+//reservation methodes
+    public function AvailableTime($id){
+        $user=ExpertAvailability::where('expert_id',$id)->where('user_id',null)->select('day','date','from','to')->get();
+        return response()->json([
+            "message"=>$user
+        ]);
+    }    
+    public function Reservation($id1,$id2,Request $request){
+        $request->validate([
+            "day"=>"required",
+            "date"=>"required",
+            "from"=>"required",
+        ]);
+        $res1=ExpertAvailability::where('expert_id',$id2)
+            ->where('day',$request->day)
+            ->where('date',$request->date)
+            ->where('from',$request->from)
+            ->first();
+        if(!($res1->user_id)){
+            $res1->user_id=$id1;
+            $res1->save();
+            return response()->json([
+                "message"=>"done",
+                "data"=>$res1
+            ]);
+        }
+        else{
+            return response()->json([
+                "message"=>"taken",
+                "data"=>$res1
+            ]);
+        }
+    }
+//rating methodes
+    public function rating(Request $request,$id1,$id2){
+        $request->validate([
+            "rate"=>"required"
+        ]);
+        $rate=new Rating();
+        $rate->user_id=$id1;
+        $rate->expert_id=$id2;
+        if(DB::table('ratings')->where('user_id',$id1)->first()&&DB::table('ratings')->where('expert_id',$id2)->first()){
+            $rate=Rating::where('expert_id',$id2)->first();
+            $rate->rate=$request->rate;
+            $rate->save();
+            $rate1=Rating::where('expert_id',$id2)->avg('rate');
+            $expert=Expert::where('id',$id2)->first();
+            $expert->rate=$rate1;
+            $expert->save();
+            return response()->json([
+                "message"=>"rate edited"
+            ]);
+        }
+        else{
+        $rate->rate=$request->rate;
+        $rate->save();
+        $rate1=Rating::where('expert_id',$id2)->avg('rate');
+        $expert=Expert::where('id',$id2)->first();
+        $expert->rate=$rate1;
+        $expert->save();
+        return response()->json([
+            "message"=>"rated"
+        ]);  
+        }
+    }       
+     public function showFavorites($id){
+        $fav = DB::table('favorites')->where('user_id',$id)
+        ->join('experts', 'experts.id', '=', 'favorites.expert_id')
+        ->select('experts.name','experts.email','experts.photo','experts.phone_number','experts.address','experts.experiences','experts.experience_years','experts.session_price','experts.rate','experts.Medical_consulting','experts.Professional_consulting','experts.Psychological_consulting','experts.Family_consulting','experts.management_consulting')
+        ->get();    
+        return response()->json([
+            "message"=>"your favorites list:",
+            "favorites"=>$fav
+        ]);
+
+    }                                                            
 }
