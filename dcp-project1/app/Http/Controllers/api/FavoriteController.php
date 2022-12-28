8e427bf8d70e88761c83d3330787bd3fe3ea5a7b
RamiH20 <120262594+RamiH20@users.noreply.github.com>
2022-12-29 02:21:03 +0300

    Update FavoriteController.php

--- dcp-project1/app/Http/Controllers/api/FavoriteController.php
+++ dcp-project1/app/Http/Controllers/api/FavoriteController.php
@@ -4,47 +4,25 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Favorite;
-use Illuminate\Http\Request;
-use App\Models\User;
-use App\Models\Expert;
 use Illuminate\Support\Facades\DB;
 class FavoriteController extends Controller
 {
-    public function AddExpert(Request $request,$id){
-        $request->validate([
-        "expert_name"=>"required",
-        "Medical_consulting"=>"",
-            "Professional_consulting"=>"",
-            "Psychological_consulting"=>"",
-            "Family_consulting"=>"",
-            "management_consulting"=>""
-        ]);
-        // craete course
+    public function AddExpert($id1,$id2){
         $favorite=new Favorite();
-        
-        $favorite->user_id=$id;
-        $favorite->expert_name=$request->expert_name;
-        $favorite->Professional_consulting=$request->Professional_consulting;
-        $favorite->Psychological_consulting=$request->Psychological_consulting;
-        $favorite->Family_consulting=$request->Family_consulting;
-        $favorite->management_consulting=$request->management_consulting;
-       
-        $favorite->save();
-        
-        // response
-        if(!$expert = Expert::where("name" , "=" , $request->expert_name)-> first()){
-
+        if(DB::table('favorites')->where('user_id',$id1)->first()&&DB::table('favorites')->where('expert_id',$id2)->first()){
             return response()->json([
-                'status'=>0,
-                'message'=>'invaild'
+                "message"=>"already added"
             ]);
         }
-        
+        $favorite->user_id=$id1;
+        $favorite->expert_id=$id2;
+        $favorite->save();
         return response()->json([
         'status'=>1,
         'message'=>'expert added successfully'
-        ],200);
-        
+        ],200); 
     } 
-    
+    public function deleteExpert($id1,$id2){
+        $favorite=Favorite::where('expert_id',$id1)->where('user_id',$id2)->delete();
+    } 
 }
